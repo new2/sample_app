1e00b8fa75cd21c849922479787001bba9db677c
New2 <new2everything@gmail.com>
2011-02-21 15:58:35 -0500

    Added a Users controler and layout link tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 SampleApp::Application.routes.draw do
   
-  root :to => "pages#home"
+  get 'users/new'
+
+  root :to => 'pages#home'
   
-  match '/contact', :to => "pages#contact"
-  match '/about',   :to => "pages#about"
-  match '/help',    :to => "pages#help"
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+  match '/signup',  :to => 'users#new'
   
   
   
