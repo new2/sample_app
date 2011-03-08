65016d85b1aa0a897f8c8835297874313bf70973
New2 <new2everything@gmail.com>
2011-03-08 14:26:44 -0500

    Working user siginin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 # == Schema Information
-# Schema version: 20110303205921
+# Schema version: 20110303224728
 #
 # Table name: users
 #
@@ -9,6 +9,7 @@
 #  created_at         :datetime
 #  updated_at         :datetime
 #  encrypted_password :string(255)
+#  salt               :string(255)
 #
 
 class User < ActiveRecord::Base
@@ -34,10 +35,13 @@ class User < ActiveRecord::Base
 
   def self.authenticate(email, submitted_password)
     user = find_by_email(email)
-    return nil if user.nil?
-    return user if user.has_password?(submitted_password)
+    (user && user.has_password?(submitted_password)) ? user : nil 
   end
   
+  def self.authenticate_with_salt(id, cookie_salt)
+    user = find_by_id(id)
+    (user && user.salt == cookie_salt) ? user : nil
+  end
   
   private
   
