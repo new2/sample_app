c16205ac24f07161cb90887f00bf7b8c370a16d6
New2 <new2everything@gmail.com>
2011-03-15 16:34:17 -0400

    Protecting edit and upate with before filter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:edit, :update]
   
   def show
     @user = User.find(params[:id])
@@ -36,4 +37,11 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+  
+  private
+  
+    def authenticate
+      deny_access unless signed_in?
+    end
+  
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -204,4 +204,23 @@ describe UsersController do
       end
     end
   end
+  
+  
+  describe "authentication of the edit/update actions" do
+    
+    before(:each) do
+      @user = Factory(:user)
+    end
+    
+    it "should deny access to 'edit'" do
+      get :edit, :id => @user
+      response.should redirect_to(signin_path)
+      flash[:notice].should =~ /sign in/i
+    end
+    
+    it "should dency access to 'update'" do
+      put :update, :id => @user, :user => {}
+      response.should redirect_to(signin_path)
+    end
+  end
 end
