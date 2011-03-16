3844a5433986a2e7db32204c71676f2bde4cfb31
New2 <new2everything@gmail.com>
2011-03-16 12:15:04 -0400

    Can now destroy users (as admins)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 # == Schema Information
-# Schema version: 20110303224728
+# Schema version: 20110316153045
 #
 # Table name: users
 #
@@ -10,6 +10,7 @@
 #  updated_at         :datetime
 #  encrypted_password :string(255)
 #  salt               :string(255)
+#  admin              :boolean
 #
 
 class User < ActiveRecord::Base
